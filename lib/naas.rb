6d11c6bce6103d57628ff94788a205ddcaa46fb6
Andrew Vos <andrew.vos@gmail.com>
2013-07-22 16:39:22 +0100

    just kinda wrote all the code I need

--- lib/naas.rb
+++ lib/naas.rb
@@ -1,5 +1,32 @@
 require "naas/version"
+require "sinatra/base"
+require 'sinatra/synchrony'
+require "json"
 
 module Naas
-  # Your code goes here...
+  class Application < Sinatra::Base
+    register Sinatra::Synchrony
+
+    get "/message/:client_id" do
+      wait_for_next_message params[:client_id]
+    end
+
+    def wait_for_next_message client_id
+      loop do
+        $messages ||= {}
+        $messages[client_id] ||= []
+        if $messages[client_id].any?
+          message = $messages[client_id].shift
+          return message.to_json
+        end
+        EM::Synchrony.sleep 0.1
+      end
+    end
+
+    post "/message/:client_id" do
+      $messages ||= {}
+      $messages[params[:client_id]] ||= []
+      $messages[params[:client_id]] << {:title => params[:title], :body => params[:body]}
+    end
+  end
 end
